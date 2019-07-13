310353aca9d0c4b75f71006b6f328ec055310d45
willy <willy.bruhn@gmx.de>
2019-07-13 18:12:19 +0200

    smart algorithm, 1000x faster

--- MetricGeometry/Experimental.R
+++ MetricGeometry/Experimental.R
@@ -913,3 +913,424 @@ length(which(closest %in% getFunctionalProteins() == TRUE))
 Emd_distance_matrix2 = readDistanceMatrix1("/home/willy/Schreibtisch/106Test/Output/quickEmd_n_100_m_3.csv")
 
 
+#--------------------------------------------------------------------------------
+# projectionMethod
+#--------------------------------------------------------------------------------
+
+
+approximateCDF <- function(F_, q){
+  # F_ ... cumulitive distribution function
+  # q ... number of equal heights to approximate F_
+  #-------------------------------------------------
+  knots = knots(F_)
+  n_F = length(knots)
+
+  # print(knots)
+    
+  F_app = rep(0,q)
+  for(i in 1:q){
+    
+    # print(n_F*i/(q+1))
+    
+    ind = n_F*i/(q+1)
+    
+    if(ind - floor(ind) > 0){
+      F_app[i] = (knots[n_F*i/(q+1)] + knots[(n_F*i+1)/(q+1)])/2
+    } else{
+      F_app[i] = knots[n_F*i/(q+1)]
+    }
+    
+    
+  }
+
+  return(F_app)  
+}
+
+plotDistWithApproximation <- function(F_,F_app){
+  
+  plot(F_)
+  
+  q = length(F_app)
+  
+  sfun = stepfun(F_app , y = c(0,seq(1/q,1,1/q)))
+  # points(x = F_app, y = seq(1/q,1,1/q))
+  plot(sfun, add = TRUE)
+}
+
+DifferenceOfIntegral_F_and_Approx <- function(F_, F_app){
+  q = length(F_app)
+  sfun = stepfun(F_app , y = c(0,seq(1/q,1,1/q)))
+  
+  DifferenceOfIntegral(F_,sfun)
+}
+
+manhattan_dist <- function(rating1, rating2){
+  distance <- abs(rating1-rating2)
+  distance <- sum(distance)
+  return(distance)
+}
+
+manhattan_dist_distribution_approx <- function(rating1, rating2){
+  return(manhattan_dist(rating1,rating2)/(length(rating1)))
+}
+
+plot2d_dist_approx <-function(pos13_F_approx_list, name=NULL, col = "red", xli = NULL, yli = NULL, add = FALSE, onlyGeo = FALSE){
+  
+  if(add == FALSE){
+    x_val = pos13_F_approx_list[[1]][1]
+    y_val = pos13_F_approx_list[[1]][2]
+    
+    if(!is.null(xli) && !is.null(yli)) {
+      plot(x = x_val, y = y_val-x_val, xlim = xli, ylim = yli,  col = col)
+    } else {
+      plot(x = x_val, y = y_val-x_val, col = col)
+    }
+  }
+
+
+  x_vals = rep(0,length(pos13_F_approx_list))
+  y_vals = rep(0,length(pos13_F_approx_list))
+  
+  for(i in 1:length(pos13_F_approx_list)){
+    x_val = pos13_F_approx_list[[i]][1]
+    y_val = pos13_F_approx_list[[i]][2]
+    
+    if(onlyGeo == FALSE) points(x = x_val, y = y_val-x_val, col = col)
+    
+    
+    x_vals[i] = x_val
+    y_vals[i] = y_val-x_val
+  }
+  
+  # plot the name
+  if(!is.null(name)){
+    geo = c(sum(x_vals)/length(x_vals), sum(y_vals)/length(y_vals))
+    
+    print(geo)
+    if(onlyGeo == TRUE) {
+      text(x = geo[1], y = geo[2], labels = c(name), cex = 2, col = col)
+    } else {
+      text(x = geo[1], y = geo[2], labels = c(name), cex = 2, col = "black")
+    }
+
+  }
+}
+
+generateF_approximations <- function(OutputPath, protName, n = 100, m = 10, q = 2, pos =TRUE){
+  
+  pos13 = read_pts_file(OutputPath = OutputPath,protName = protName, pos = pos)
+  
+  pos13_F_list = list()
+  pos13_F_approx_list = list()
+  
+  for(i in 1:m){
+    pos13_F_list[[i]] = samplePointsAndCalculateCDFofEc(all_pts = pos13,n = n,plot = FALSE)
+    pos13_F_approx_list[[i]] = approximateCDF(pos13_F_list[[i]],q)
+  }
+  
+  return(list("F_list" = pos13_F_list, "F_app_list" = pos13_F_approx_list))
+}
+
+getDistanceMatrixFromProjection <- function(df){
+  
+  q= ncol(df)-1
+  
+  distances = as.matrix(dist(x = df[,2:q], diag = TRUE, upper = TRUE),method ="manhattan")
+  colnames(distances) = df[,1]
+  rownames(distances) = df[,1]
+  
+  return(distances)
+}
+
+getGeometricCenters <- function(df){
+
+  names = unique(df[,1])
+  
+  geos = data.frame(matrix(0, nrow = length(names), ncol = 3))
+  colnames(geos) = c("name", "x", "y")
+  
+  for(i in 1:length(names)){
+    inds = which(df[,1] == names[i])
+    
+    x = sum(df[inds,2])/length(df[inds,2])
+    y = sum(df[inds,3])/length(df[inds,3])
+    geos[i,1] = names[i]
+    geos[i,2] = x
+    geos[i,3] = y
+  }
+  
+  return(geos)
+}
+#-------------------------------------------------------------------
+## Example
+
+calcError <- function(all_pts, n = 100, q = 2, plot = TRUE, verbose = TRUE){
+  # q = 2
+  # n = 100
+  pos13_F1 = samplePointsAndCalculateCDFofEc(all_pts = all_pts,n = n,plot = plot)
+  F_app1 = approximateCDF(pos13_F1,q)
+  plotDistWithApproximation(pos13_F1,F_app1)
+  DifferenceOfIntegral_F_and_Approx(pos13_F1,F_app1)
+  
+  pos13_F2 = samplePointsAndCalculateCDFofEc(all_pts = all_pts,n = n,plot = plot)
+  F_app2 = approximateCDF(pos13_F2,q)
+  plotDistWithApproximation(pos13_F2,F_app2)
+  DifferenceOfIntegral_F_and_Approx(pos13_F2,F_app2)
+  
+  DE_exact = DifferenceOfIntegral(pos13_F1,pos13_F2)
+  DE_approx = manhattan_dist_distribution_approx(F_app1,F_app2)
+  
+  if(verbose){
+    print(paste("DE_exact = ", DE_exact, ", DE_approx = ", DE_approx, sep =""))
+    print(paste("Error =", abs(DE_exact- DE_approx)))
+  }
+
+  return(abs(DE_exact- DE_approx))
+}
+
+
+calcErrors <- function(all_pts, times = 100, n = 100, q = 2, plot = FALSE){
+  errors = rep(0,times)
+  for(i in 1:times){
+    errors[i] = calcError(all_pts, n, q, plot = plot, verbose = FALSE)
+  }
+  return(errors)
+}
+
+caclErrorCurve <- function(all_pts, times = 100, n = 100, maxQ = 99, plot = FALSE){
+  meanErrors = rep(0,maxQ)
+  for(i in 1:maxQ){
+    print(i)
+    meanErrors[i] = mean(calcErrors(pts, times = times, n = n, q = i))
+  }
+  
+  return(meanErrors)
+}
+
+pts = read_pts_file(OutputPath = OutputPath, protName = "000_Trx")
+
+errors = calcErrors(pts, times = 100, n = 100, q = 2)
+boxplot(errors, ylim = c(0,1))
+
+errors = calcErrors(pts, times = 100, n = 100, q = 10)
+boxplot(errors, ylim = c(0,1))
+
+errors = calcErrors(pts, times = 100, n = 100, q = 99)
+boxplot(errors, ylim = c(0,1))
+
+meanErrorsQ = caclErrorCurve(pts, times = 100, n = 3, maxQ = 2)
+
+plot(x = c(1:length(meanErrorsQ)), y = meanErrorsQ, type ="l", xlab = "q", ylab = "mean(Error)", main = "abs(DE_exact - DE_approx)")
+
+
+
+calcError(pts,q = 10)
+
+#-------------------------------------------------------------------
+
+
+
+getAll_protein_F_approximations <- function(OutputPath,  n = 100, m = 50, q = 2, pos = TRUE){
+  print(paste("Loading from ", OutputPath, sep =""))
+  protein_names = list.dirs(OutputPath, recursive = FALSE, full.names = FALSE)
+  
+  distributions_lists = list()
+  
+  for(i in 1:length(protein_names)){
+    F_app = generateF_approximations(OutputPath = OutputPath, protName = protein_names[i], n = n, m = m, q = q, pos =pos)
+    distributions_lists[[i]] =  list("name" = protein_names[i],"F" = F_app)
+  }
+  
+  return(distributions_lists)
+}
+
+get_x_and_y_vals <- function(all_protein_F_approximations,m){
+  x_vals = rep(0,length(all_protein_F_approximations)*m)
+  y_vals = rep(0,length(all_protein_F_approximations)*m)
+  
+  for(i in 1:length(all_protein_F_approximations)){
+    for(j in 1:m){
+      ind = (i-1)*m
+      x_vals[ind+j] = all_protein_F_approximations[[i]]$F$F_app_list[[j]][1]
+      y_vals[ind+j] = all_protein_F_approximations[[i]]$F$F_app_list[[j]][2]-all_protein_F_approximations[[i]]$F$F_app_list[[j]][1]
+    }
+  }
+  
+  return(list("x_vals" = x_vals, "y_vals" = y_vals))
+}
+
+plot_all_F_approximations <- function(all_protein_F_approximations, colors = NULL, onlyGeo = FALSE, xli = NULL, yli = NULL){
+  
+  n_proteins = all_protein_F_approximations
+  m = length(all_protein_F_approximations[[1]]$F$F_app_list)
+
+  if(is.null(xli) || is.null(yli)){
+    obj = get_x_and_y_vals(all_protein_F_approximations,m)
+    x_vals = obj$x_vals
+    y_vals = obj$y_vals
+    
+    xli = c(min(x_vals),max(x_vals))
+    yli = c(min(y_vals),max(y_vals))
+  }  
+
+  if(is.null(colors)) colors = rep("black", length(n_proteins))
+  
+  plot2d_dist_approx(all_protein_F_approximations[[1]]$F$F_app_list, xli = xli, yli = yli, col = colors[1], name = all_protein_F_approximations[[1]]$name,onlyGeo = onlyGeo)
+  for(i in 2:length(all_protein_F_approximations)){
+    
+    print(i)
+    plot2d_dist_approx(all_protein_F_approximations[[i]]$F$F_app_list, add = TRUE, col = colors[i], name = all_protein_F_approximations[[i]]$name, onlyGeo = onlyGeo)
+  }
+
+}
+
+getManhattanProjection2d <- function(all_protein_F_approximations){
+  prot_number = length(all_protein_F_approximations)
+  
+  m = length(all_protein_F_approximations[[1]]$F$F_app_list)
+  
+  df = data.frame(matrix(0,nrow = m*prot_number, ncol = 3))
+  colnames(df) = c("name", "x", "y")
+  
+  for(i in 1:prot_number) {
+    print(all_protein_F_approximations[[i]]$name)
+    name = all_protein_F_approximations[[i]]$name
+    
+    ind = (i-1)*m
+    for(j in 1:m){
+      df[ind+j,1] = name
+      df[ind+j,2] = all_protein_F_approximations[[i]]$F$F_app_list[[j]][1]
+      df[ind+j,3] = all_protein_F_approximations[[i]]$F$F_app_list[[j]][2]-  all_protein_F_approximations[[i]]$F$F_app_list[[j]][1]
+    }
+    
+  }
+  
+  return(df)
+}
+
+length(all_protein_F_approximations[[1]]$F$F_app_list[[1]])
+
+getManhattanProjection <- function(all_protein_F_approximations){
+  prot_number = length(all_protein_F_approximations)
+  
+  m = length(all_protein_F_approximations[[1]]$F$F_app_list)
+  q = length(all_protein_F_approximations[[1]]$F$F_app_list[[1]])
+  
+  df = data.frame(matrix(0,nrow = m*prot_number, ncol = q+1))
+  colnames(df) = c("name", colnames(df)[2:q+1])
+  
+  for(i in 1:prot_number) {
+    print(all_protein_F_approximations[[i]]$name)
+    name = all_protein_F_approximations[[i]]$name
+    
+    ind = (i-1)*m
+    for(j in 1:m){
+      df[ind+j,1] = name
+      
+      df[ind+j,1+1] = all_protein_F_approximations[[i]]$F$F_app_list[[j]][1]
+      for(k in 2:q){
+        df[ind+j,k+1] = all_protein_F_approximations[[i]]$F$F_app_list[[j]][k] - df[ind+j,q]
+      }
+
+    }
+    
+  }
+  
+  return(df)
+}
+
+
+quickRepSampling <- function(OutputPath, distance_path, fName, pos = TRUE, n = 100, m = 22, q = 21){
+  # actually q should be possible to be larger than n
+  #
+  #-------------------------------------------------------
+  
+  all_protein_F_approximations = getAll_protein_F_approximations(OutputPath = OutputPath, m = m, q = q, n = n, pos = TRUE)
+  
+  df = getManhattanProjection(all_protein_F_approximations)
+  distances = getDistanceMatrixFromProjection(df)
+  
+  geos = getGeometricCenters(df)
+  
+  geoDistances = getDistanceMatrixFromProjection(geos)
+  
+  # can be loaded with loadDistanceMatrix1()
+  write.csv(geoDistances,file = paste(distance_path,"/",fName, ".csv",sep =""))
+}
+
+
+quickRepSampling(OutputPath = "/home/willy/Schreibtisch/106Test/Output/", distance_path = "/home/willy/Schreibtisch/106Test/Output/", fName = "distTest")
+
+library(rbenchmark)
+benchmark("quickRepSampling" = quickRepSampling(OutputPath = "/home/willy/Schreibtisch/106Test/Output/", distance_path = "/home/willy/Schreibtisch/106Test/Output/", fName = "distTest"),
+          "sapply" = quickRepSampling(OutputPath = "/home/willy/Schreibtisch/106Test/Output/", distance_path = "/home/willy/Schreibtisch/106Test/Output/", fName = "distTest"),replications = 2,
+          columns = c("test", "replications", "elapsed",
+                      "relative", "user.self", "sys.self"))
+
+n = 4
+m = 100
+q = 3
+times = 2
+OutputPath = "/home/willy/Schreibtisch/106Test/Output/"
+all_protein_F_approximations = getAll_protein_F_approximations(OutputPath = OutputPath, m = m, q = q, n = n, pos = TRUE)
+
+functionals = c(getFunctionalProteins(),"000_Trx")
+colors = rep("blue", length(all_protein_F_approximations))
+for(i in 1:length(all_protein_F_approximations)){
+  if(all_protein_F_approximations[[i]]$name %in% functionals) colors[i] = "red"
+}
+
+# plot_all_F_approximations(all_protein_F_approximations, colors = colors, onlyGeo = TRUE, xli = c(16, 22), yli = c(1,6))
+
+
+plot_all_F_approximations(all_protein_F_approximations, colors = colors, onlyGeo = FALSE)
+
+
+
+# df = getManhattanProjection2d(all_protein_F_approximations)
+df = getManhattanProjection(all_protein_F_approximations)
+distances = getDistanceMatrixFromProjection(df)
+
+geos = getGeometricCenters(df)
+
+geos[7,]
+
+xl = geos[7,2] + c(-0.1,1) 
+yl = geos[7,3] + c(-1,1) 
+unique(geos[intersect(intersect(intersect(which(geos[,2] < xl[2]), which(geos[,2] > xl[1])),which(geos[,3] < yl[2])), which(geos[,3] > yl[1])),1])
+
+geoDistances = getDistanceMatrixFromProjection(geos)
+
+geoDistances[7,]
+
+Emd_distances = emd_parallel(DE_parallel = distances,m = m)
+
+
+emdRef = readDistanceMatrix3(file = "/home/willy/Schreibtisch/106Test/Output/")
+cor(c(Emd_distances[-106,-106]),c(emdRef))
+
+
+closest = rep(0,106)
+for(i in 1:length(closest)){
+  closest[i] = length(which(rownames(geoDistances)[which.minn(geoDistances[7,],n = i)] %in% functionals) == TRUE)
+}
+
+emdRef = readDistanceMatrix2()
+closest2 = rep(0,105)
+for(i in 1:length(closest2)){
+  closest2[i] = length(which(rownames(emdRef)[which.minn(emdRef[7,],n = i)] %in% functionals) == TRUE)
+}
+plot(closest2, type = "l", main = "closest neihgbours that are functional", xlab ="NN", ylab = "functionals found", col = "blue")
+points(closest, type = "l", main = "closest neihgbours that are functional", xlab ="NN", ylab = "functionals found", col = "red")
+legend(x = 35, y = 10,legend = c("RepSamp", "QuickRepSamp"),lty = c(1,1), col = c("blue", "red"))
+
+
+
+
+
+
+
+
+
+
+
